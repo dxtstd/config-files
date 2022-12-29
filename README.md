286229bdd33e5f7639860b75db648f5b64f0ed81
dxtstd <denta123.ft@gmail.com>
2022-12-30 07:09:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#### Config Files
+this is for personal use, don't run anything, change any file on your computer with all the files in this repo
+
---
+++ setup_vps.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+welcome="
+VPS SETUP
+Ubuntu 22.04
+
+~/dxtstd
+"
+
+### WHEN $PREFIX IS UNDEFINED
+if ! [ $PREFIX ]
+then
+    PREFIX='/usr'
+fi
+### WHEN $TMPDIR IS UNDEFINED
+if ! [ $TMPDIR ]
+then
+    TMPDIR='/tmp'
+fi
+
+if [ -e "${PREFIX}/bin/figlet" ]
+then
+    figlet $welcome
+else
+    echo "\"$welcome\""
+    echo
+fi
+
+DEVICE_OS=$(uname -o)
+DEVICE_ARCH=$(uname -m)
+echo "Detected OS: ${DEVICE_OS}"
+echo "Detected Arch: ${DEVICE_ARCH}"
+echo ""
+
+if [ $UID != 0 ];
+then
+  echo "you must run this script on root user..."
+  exit
+fi
+
+### execute setuping VPS
+
+PACKAGE="git ffmpeg sox libsox-fmt-all"
+install_package () {
+    echo "Update repo list & Upgrade Packages..."
+    apt update && apt upgrade -y 
+
+    echo
+    echo "Installing \"$PACKAGE\" package... "
+    apt install $PACKAGE -y 
+
+    echo
+}
+install_package
+
+## For NodeJS
+URL_NODEJS=""
+NODEJS_LINUX() {
+    NODEJS_WEB="https://nodejs.org/dist"
+    NODEJS_VERSION="v19.3.0"
+    NODEJS_ARCH="x64"
+    NODEJS_PLATFORM="linux"
+
+    if { [ $DEVICE_ARCH == 'amd64' ] || [ $DEVICE_ARCH == 'x86_64' ]; }
+    then
+        NODEJS_ARCH='x64'
+        else if { [ $DEVICE_ARCH == 'aarch64' ]; }
+        then
+            NODEJS_ARCH='arm64'
+            else if { [ $DEVICE_ARCH == 'aarch32' ] || [ $DEVICE_ARCH == 'armv8l' ] || [ $DEVICE_ARCH == 'armv7l' ]; }
+            then
+                NODEJS_ARCH='armv7l'
+            fi
+        fi
+    fi
+    NODEJS_PACKAGE="node-${NODEJS_VERSION}-${NODEJS_PLATFORM}-${NODEJS_ARCH}.tar.gz"
+    URL_NODEJS="${NODEJS_WEB}/${NODEJS_VERSION}/${NODEJS_PACKAGE}"
+}
+
+
+install_nodejs () {
+  read -p "Install a NodeJS? [Y/n]: " ANSWER_NJS_INSTALL
+  case "$ANSWER_NJS_INSTALL" in
+    [Yy][Ee][Ss]|[Yy])
+      NODEJS_LINUX
+      echo URl NodeJS: "$URL_NODEJS"
+
+      echo 'Downloading NodeJS...'
+      wget -O $TMPDIR/nodejs.tar.gz $URL_NODEJS
+      echo 'Extract NodeJS...'
+      mkdir $TMPDIR/nodejs
+      tar xfz $TMPDIR/nodejs.tar.gz -C $TMPDIR/nodejs
+      echo "Move File To $PREFIX..."
+      cp -rf $TMPDIR/nodejs/*/* $PREFIX
+      echo "Remove TMP NodeJS..."
+      rm -rf $TMPDIR/node*
+
+      NPM_MODULE="ts-node typescript nodemon pm2"
+      echo "Installing NPM Module $NPM_MODULE (Global)..."
+      npm install -g $NPM_MODULE
+
+      echo
+    ;;
+    [Nn][Oo]|[Nn])
+      
+    ;;
+    *)
+      install_nodejs
+    ;;
+  esac
+}
+install_nodejs
+
+VNC_CONFIG="
+#!/bin/sh
+
+### FOR AUDIO
+#PULSE_SERVER=127.0.0.1
+pulseaudio --start
+
+dbus-launch --exit-with-session startxfce4
+"
+
+VSCODE_URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+ask_install_desktop () {
+    read -p "Install a Desktop Environment? [Y/n]: " ANSWER_DE_INSTALL
+    case "$ANSWER_DE_INSTALL" in
+        [Yy][Ee][Ss]|[Yy])
+            echo "Installing Package DE..."
+            apt install xfce4 xfce4-goodies tigervnc-standalone-server -y
+            
+            echo "Fetch Chrome, VSCode..."
+            mkdir $TMPDIR/deb-package
+            wget -O $TMPDIR/deb-package/VSCODE.deb $VSCODE_URL
+            wget -O $TMPDIR/deb-package/CHROME.deb $CHROME_URL
+            echo "Installing Chrome, VSCode..."
+            apt install $TMPDIR/deb-package/*.deb -y
+            rm -rf $TMPDIR/deb-package
+            
+#            echo "Configuring VNC File..."
+#            for USER_HOME_DIR in /home/*; do
+#              echo $USER_HOME_DIR
+#              if [ ! -d $USER_HOME_DIR/.vnc ]
+#              then
+#                  mkdir $USER_HOME_DIR/.vnc
+#              fi
+#              echo $VNC_CONFIG > $USER_HOME_DIR/.vnc/xstartup
+#             chmod +x $USER_HOME_DIR/.vnc/xstartup
+#
+#              USER_array=(`echo $USER_HOME_DIR | sed 's/\//\n/g'`)
+#              chown $USER_array[2]:$USER_array[2] $USER_HOME_DIR/.vnc/xstartup
+#            done
+            
+            echo
+        ;;
+        [Nn][Oo]|[Nn])
+            
+        ;;
+        *)
+          ask_install_desktop
+        ;;
+    esac
+}
+ask_install_desktop
+
+config_pulseaudio () {
+  echo "Configuring pulseaudio..."
+  if [ -e "/etc/pulse/default.pa" ];
+  then
+      cp -f ./default.pa /etc/pulse/default.pa
+      
+  else
+      echo "Pulseaudio not found..."
+  fi
+}
+config_pulseaudio
